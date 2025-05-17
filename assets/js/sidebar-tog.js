ce0d80db0d40b1f63c4453cfdfd9785b8d09a4e3
Mahm0ud-Eid <114058268+Mahm0ud-Eid@users.noreply.github.com>
2025-05-17 16:13:11 +0300

    Refactor CSS and JavaScript for sidebar functionality and styling
    - Updated file-upload.css to comment out unnecessary margin properties.
    - Modified sidebar-tog.js to streamline sidebar toggle logic and commented out unused code.
    - Enhanced admin-responsive.css with improved sidebar and main content responsiveness, including adjustments to padding and margins.
    - Commented out unused styles in style.css to clean up the codebase.

--- assets/js/sidebar-tog.js
+++ assets/js/sidebar-tog.js
@@ -3,16 +3,25 @@ let sbTog = document.querySelector(".sd-tog");
 sbTog.addEventListener("click", function () {
   let sidebar = document.querySelector(".sb2-1");
   let mainContent = document.querySelector(".sb2-2");
+  let path = document.querySelector(".path");
 
-  if (sidebar.classList.contains("active")) {
-    sidebar.classList.remove("active");
-    mainContent.classList.remove("shifted");
-    sbTog.classList.remove("expand");
+  if (window.innerWidth > 992) {
+    sidebar.classList.toggle("active");
+    mainContent.classList.toggle("shifted");
   } else {
-    sidebar.classList.add("active");
-    sbTog.classList.add("expand");
-    if (window.innerWidth > 992) {
-      mainContent.classList.add("shifted");
-    }
+    sidebar.classList.toggle("active");
+    // path.classList.toggle("shifted");
   }
+
+  // if (sidebar.classList.contains("active")) {
+  //   sidebar.classList.remove("active");
+  //   mainContent.classList.remove("shifted");
+  //   sbTog.classList.remove("expand");
+  // } else {
+  //   sidebar.classList.add("active");
+  //   sbTog.classList.add("expand");
+  //   if (window.innerWidth > 992) {
+  //     mainContent.classList.add("shifted");
+  //   }
+  // }
 });
