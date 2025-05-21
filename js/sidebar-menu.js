338ebb466f1c630c6fc0182b63dab655398ce79a
Mohamed Ahmed <m77md.107@gmail.com>
2025-05-22 01:22:47 +0300

    Refactor event management: remove old event edit page, update navigation to events, and create new event edit page

---
+++ js/sidebar-menu.js
@@ -0,0 +1,9 @@
+/**
+ * Sidebar Collapsible Menu Initialization
+ * This script correctly initializes the collapsible sidebar menu
+ * used across all admin pages of the courses platform.
+ */
+$(document).ready(function() {
+  // Initialize the Materialize collapsible component
+  $('.collapsible').collapsible();
+});
