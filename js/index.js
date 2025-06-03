62f2e93300c1fb87faf05666df190a8bc0907b39
Mahm0ud-Eid <114058268+Mahm0ud-Eid@users.noreply.github.com>
2025-06-03 19:39:28 +0300

    feat: Add Firebase initialization and Firestore setup

---
+++ js/index.js
@@ -0,0 +1,35 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+import {
+  getAuth,
+  onAuthStateChanged,
+} from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  getDoc,
+} from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCaUETFsFnRgSmNFIwEgyGWugfrK9zHbM0",
+  authDomain: "uccd-f607e.firebaseapp.com",
+  projectId: "uccd-f607e",
+  storageBucket: "uccd-f607e.firebasestorage.app",
+  messagingSenderId: "1037776187244",
+  appId: "1:1037776187244:web:7d02b5edc4908dad390d49",
+  measurementId: "G-B95Y1W5QWP",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const users = collection(db, "users");
+const snapshot = await getDocs(users);
+
+export { auth, db };
