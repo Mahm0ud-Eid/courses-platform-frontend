008ff1ac7b443a3fcabec0839bdcb2ba6cd8954c
Mahm0ud-Eid <114058268+Mahm0ud-Eid@users.noreply.github.com>
2025-06-19 16:08:00 +0300

    feat: initialize student.js with user data retrieval from session storage

---
+++ js/student.js
@@ -0,0 +1,10 @@
+import { auth, db, validateToken, collection } from "../js/index.js"; // Import auth and db from index.js
+
+import {
+  doc,
+  getDoc,
+} from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
+
+// ...existing code...
+const userData = JSON.parse(sessionStorage.getItem("userData"));
+console.log(userData);
