ff571f09764ec66aae04a11c9a5e467c0d0e97ca
Mahm0ud-Eid <114058268+Mahm0ud-Eid@users.noreply.github.com>
2025-06-19 16:16:05 +0300

    feat: enhance DOMContentLoaded event to display user data from session storage

--- js/student.js
+++ js/student.js
@@ -5,9 +5,17 @@ import {
   getDoc,
 } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
 
-// ...existing code...
 const userData = JSON.parse(sessionStorage.getItem("userData"));
+
+let stdName = document.querySelector(".std-name");
+let stdId = document.querySelector(".std-id");
 
 document.addEventListener("DOMContentLoaded", function () {
   validateToken();
+  if (userData) {
+    stdName.textContent = userData.name;
+    stdId.textContent = userData.id;
+  } else {
+    console.error("No user data found in sessionStorage.");
+  }
 });
