60f6a1ee1809559d353a5bc8f852ebd4eb1347b8
Mahm0ud-Eid <114058268+Mahm0ud-Eid@users.noreply.github.com>
2025-06-21 18:11:36 +0300

    feat: add firebase configuration file for project setup

---
+++ js/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCaUETFsFnRgSmNFIwEgyGWugfrK9zHbM0",
+  authDomain: "uccd-f607e.firebaseapp.com",
+  projectId: "uccd-f607e",
+  storageBucket: "uccd-f607e.firebasestorage.app",
+  messagingSenderId: "1037776187244",
+  appId: "1:1037776187244:web:7d02b5edc4908dad390d49",
+  measurementId: "G-B95Y1W5QWP",
+};
+export { firebaseConfig };
